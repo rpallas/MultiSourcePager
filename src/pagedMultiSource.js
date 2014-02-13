e5e1d40e29fe3060f9f3ea2bb06d84ec8d2566a4
Robbie Pallas <rpallas@gmail.com>
2014-02-13 17:33:12 +0000

    It gets the next batch of records from each source

--- src/pagedMultiSource.js
+++ src/pagedMultiSource.js
@@ -1,19 +1,48 @@
 var PagedMultiSource = (function() {
     "use strict";
 
-    var Module = function(){
+    var Module = function(limit){
         var self = this;
-        self.sources = [];
-        self.getSources = function(url, callback) {
-            $.ajax({
-                type:"GET",
-                url:url
+        self.limit = limit;     // Total items in a page
+        self.sources = [];      // Storage for the list of api sources
+        self.totalRecords = 0;
+        self.recordBuffer = []; // in memory record store
+
+        /**
+         * Gets a list of api sources
+         * @param url the endpoint used for discovering the api sources
+         */
+        self.getSources = function(url) {
+            return $.ajax({
+                type: "GET",
+                url: url
             }).done(function(response){
                 self.sources = JSON.parse(response).products;
-                if (callback) callback();
             });
         };
+
+        /**
+         * Gets the next batch of data from each source
+         * @return JQuery Deferred
+         */
+        self.getNext = function(){
+            var count = 0, requests = [], records = [];
+            for (var i=0, total=self.sources.length; i<total; i++){
+                requests.push($.ajax({
+                    type: "GET",
+                    url: self.sources[i].link,
+                    data: { limit: limit }
+                }).done(function(response){
+                    var data = JSON.parse(response);
+                    $.merge(self.recordBuffer, data.data);
+                    self.totalRecords += data.total;
+                }));
+            }
+            return $.when.apply($, requests);
+        }
     };
+
+
 
     return Module;
 }());
--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,10 @@
+var dataSource, limit;
+
 module('PagedMultiSource', {
+    setup: function() {
+        limit = 25;
+        dataSource = new PagedMultiSource(limit);
+    },
     teardown: function() {
         // clean up after each test
         $.mockjaxClear();
@@ -9,24 +15,72 @@ module('PagedMultiSource', {
  * Tests
  */
 
-test('It has a working test harness', function() {
+test("It has a working test harness", function() {
     notEqual(true, false, "We're up and running...");
+});
+
+test("It sets the correct limit (pagesize)", function() {
+    equal(dataSource.limit, limit, "The correct limit was set");
 });
 
 asyncTest("It retrieves a list of REST endpoints", function(){
     // Arrange
+    ajaxMockIndexWith2Sources();
+    ajaxMockAnySourceWith1Record();
+    // Act
+    dataSource.getSources("/index.php")
+        .done(function(){
+            // Assert
+            equal(dataSource.sources.length, 2, "a list of endpoints was stored with correct length");
+            start();
+        });
+});
+
+asyncTest("It gets the next batch of records from each source", function(){
+    // Arrange
+    ajaxMockAnySourceWith1Record();
+    dataSource.sources = get2Sources();
+    // Act
+    dataSource.getNext()
+        .done(function(){
+            // Assert
+            equal(dataSource.totalRecords, 2, "correct number of total records was set");
+            equal(dataSource.recordBuffer.length, 2, "The correct number of records was stored in the buffer")
+            start();
+        });
+});
+
+/**
+ * Utilities
+ */
+
+function ajaxMockIndexWith2Sources(){
     $.mockjax({ url:"/index.php", responseTime: 50,
-        responseText:JSON.stringify({ products:[
-            {name:"TechTest", "link":"http://randomstorm.dev/JS/server1.php"},
-            {name:"TechTest", "link":"http://randomstorm.dev/JS/server2.php"}
+        responseText: JSON.stringify({ products: get2Sources() })
+    });
+}
+
+function ajaxMockAnySourceWith1Record(){
+    $.mockjax({ url:"http://randomstorm.dev/JS/server*", responseTime: 50, data: { limit: limit },
+        responseText:JSON.stringify({"total":1, "offset":0, "limit":1, "data":[
+            {"name":"ET USER_AGENTS suspicious user agent string (changhuatong)", "date":"2013-09-05 23:31:35"}
         ]})
     });
-    var ds = new PagedMultiSource();
-    // Act
-    ds.getSources("/index.php", function(){
-        // Assert
-        ok(true, "an ajax call was made");
-        equal(ds.sources.length, 2, "a list of endpoints was stored with correct length");
-        start();
-    });
-});
+}
+
+function get2Sources(){
+    return [
+        {name:"TechTest", "link":"http://randomstorm.dev/JS/server1.php"},
+        {name:"TechTest", "link":"http://randomstorm.dev/JS/server2.php"}
+    ];
+}
+
+//function ajaxMockAnySourceWith1Record(){
+//    $.mockjax({ url:"http://randomstorm.dev/JS/server*", responseTime: 0, data: { limit: limit },
+//        responseText: function(settings){
+//            return {"total":1, "offset":0, "limit":1, "data":[
+//                {"name":"ET USER_AGENTS suspicious user agent string (changhuatong)", "date":"2013-09-05 23:31:35"}
+//            ]};
+//        }
+//    });
+//}
